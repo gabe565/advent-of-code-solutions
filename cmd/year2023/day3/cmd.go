14a5cb3548142d7f80f27d9ff028ec42bdb28b3f
Gabe Cook <gabe565@gmail.com>
2025-01-06 00:05:51 -0600

    fix: Fix 2023 day 3 not being added as a subcommand

--- cmd/year2023/day3/cmd.go
+++ cmd/year2023/day3/cmd.go
@@ -9,7 +9,7 @@ import (
 
 func New() *day.Day[*Schematic, int] {
 	return &day.Day[*Schematic, int]{
-		Date: time.Date(2023, 12, 1, 0, 0, 0, 0, time.Local),
+		Date: time.Date(2023, 12, 3, 0, 0, 0, 0, time.Local),
 		Parse: func(r io.Reader) (*Schematic, error) {
 			var schematic Schematic
 			err := schematic.Decode(r)
